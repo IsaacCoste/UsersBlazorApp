23de36b56d5cfd82bc9ef24b8b4b7a6928f7579d
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-07-07 23:25:53 -0400

    Agrege el UserResponse a Dts para utilizarlo en el controller

---
+++ UserBlazorApp.API/Dts/User/UserResponse.cs
@@ -0,0 +1,13 @@
+﻿using UserBlazorApp.API.Dts.Role;
+
+namespace UserBlazorApp.API.Dts.User;
+public class UserResponse
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? PasswordHash { get; set; }
+    public string? PhoneNumber { get; set; }
+    public DateTimeOffset? LockoutEnd { get; set; }
+    public ICollection<RoleResponse> Role {  get; set; } = new List<RoleResponse>();
+}
