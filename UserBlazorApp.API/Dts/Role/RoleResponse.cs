24d2dd03fc585db82dee3155f2be220aa4f1d9f4
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-07-07 23:24:55 -0400

    Agrege el RoleResponse a Dts para utilizarlo en el controller

---
+++ UserBlazorApp.API/Dts/Role/RoleResponse.cs
@@ -0,0 +1,9 @@
+﻿using UserBlazorApp.API.Dts.Claims;
+
+namespace UserBlazorApp.API.Dts.Role;
+public class RoleResponse
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public ICollection<RoleClaimResponse> RoleClaims { get; set; } = new List<RoleClaimResponse>();
+}
