c0f45f2c992b40906ca083eaf29c04d422b9e74d
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-07-07 23:20:05 -0400

    Agrege una interface para el Service del cliente

---
+++ UsersBlazorApp.Data/Interfacez/CService.cs
@@ -0,0 +1,9 @@
+﻿namespace UsersBlazorApp.Data.Interfacez;
+public interface CService<C>
+{
+    Task<List<C>> GetAll();
+    Task<C> Get(int id);
+    Task<C> Add(C property);
+    Task<bool> Update(C property);
+    Task<bool> Delete(int id);
+}
