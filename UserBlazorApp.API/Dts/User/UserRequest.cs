3525da14854cb393c76d7c1f81d5bc51089ef66b
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-07-07 23:25:26 -0400

    Agrege el UserRequest a Dts para utilizarlo en el controller

---
+++ UserBlazorApp.API/Dts/User/UserRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace UserBlazorApp.API.Dts.User;
+public class UserRequest
+{
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? PasswordHash { get; set; }
+    public string? PhoneNumber { get; set; }
+    public DateTimeOffset? LockoutEnd { get; set; }
+}
