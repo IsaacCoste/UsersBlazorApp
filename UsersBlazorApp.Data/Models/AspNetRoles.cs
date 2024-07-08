e181d05cb83e9d26f84e9f8f16a3a217d4f5e81d
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-07-07 23:19:06 -0400

    Modifique el model de Roles

--- UsersBlazorApp.Data/Models/AspNetRoles.cs
+++ UsersBlazorApp.Data/Models/AspNetRoles.cs
@@ -1,23 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
 namespace UsersBlazorApp.Data.Models;
-
 public partial class AspNetRoles
 {
     [Key]
     public int Id { get; set; }
 
     [StringLength(256)]
-    public string? Name { get; set; }
-
-    [StringLength(256)]
-    public string? NormalizedName { get; set; }
-
-    public string? ConcurrencyStamp { get; set; }
+    public string? Name { get; set; }  
 
     [InverseProperty("Role")]
     public virtual ICollection<AspNetRoleClaims> AspNetRoleClaims { get; set; } = new List<AspNetRoleClaims>();
