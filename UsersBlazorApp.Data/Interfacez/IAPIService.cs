80769a358884b72dfeb48b9e55c4379d9daca0fd
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-07-07 23:20:45 -0400

    Agrege una interface para utiliarla en el service de la Api

---
+++ UsersBlazorApp.Data/Interfacez/IAPIService.cs
@@ -0,0 +1,9 @@
+﻿namespace UsersBlazorApp.Data.Interfacez;
+public interface IAPIService<T>
+{
+    Task<List<T>> GetAll();
+    Task<T> Get(int id);
+    Task<T> Add(T user);
+    Task<bool> Update(T user);
+    Task<bool> Delete(int id);
+}
